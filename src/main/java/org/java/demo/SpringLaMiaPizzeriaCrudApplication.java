05f79cde272c5a0ded4718c98d94461e7eb178d2
Marco 9726 <marcosarra9@gmail.com>
2023-05-30 18:45:38 +0200

    init commit

---
+++ src/main/java/org/java/demo/SpringLaMiaPizzeriaCrudApplication.java
@@ -0,0 +1,19 @@
+package org.java.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLaMiaPizzeriaCrudApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLaMiaPizzeriaCrudApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("HELOOOOOO");
+	}
+}
---
+++ src/main/java/org/java/demo/controller/Pizza.java
@@ -0,0 +1,70 @@
+package org.java.demo.controller;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Pizza {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String name;
+	private String description;
+	private String imgUrl;
+	private int price;
+	
+	public Pizza () {}
+	
+	public Pizza(String name, String description, String imgUrl, int price) {
+		
+		setName(name);
+		setDescription(description);
+		setImgUrl(imgUrl);
+		setPrice(price);
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+}
